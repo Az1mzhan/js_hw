4dc275a89270f24e6fd4df59e2d3d7004ad92a44
Az1mzhan <78950175+Az1mzhan@users.noreply.github.com>
2023-03-14 18:18:38 +0600

    Add files via upload

---
+++ lecture28/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react';
+// import logo from './logo.svg';
+import {SearchComponent} from "./components/SearchComponent";
+import './App.css';
+
+const App = () => {
+    return (
+      <div className="App">
+          <SearchComponent/>
+      </div>
+    );
+}
+
+export default App;
